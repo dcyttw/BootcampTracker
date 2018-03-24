a0e6578bd6203c97cfd38166c663fdef0359b735
Quintessa Anderson <quintessaanderson@Quintessas-MacBook-Pro.local>
2018-03-24 14:01:45 -0500

    worked on mysql table for reviews, created get and post api routes

---
+++ models/review.js
@@ -0,0 +1,55 @@
+module.exports = function(sequelize, DataTypes) {
+    var Review = sequelize.define("Review", {
+      ReviewID: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true
+      },
+
+      bootcampName: {
+          type: DataTypes.STRING,
+          allowNull: false,
+
+      },
+
+      curriculum: {
+        type: DataTypes.INTEGER,
+        allowNull: false
+      },
+
+      instructor: {
+        type: DataTypes.INTEGER,
+        allowNull: false
+      },
+
+      jobAssistance: {
+        type: DataTypes.INTEGER,
+        allowNull: true
+      }, 
+
+      jobFound: {
+          type: DataTypes.STRING,
+          allowNull: false,
+      },
+
+      jobPosition: {
+          types: DataTypes.STRING,
+          allowNull: false,
+
+      },
+
+      salaryRange: {
+          types: DataTypes.INTEGER,
+          allowNull: false
+          
+      },
+
+    //   overallExperience: {
+      // NEED TO RESEARCH HOW TO CALCULATE AVERAGE
+    //   }
+
+    });
+
+    return Review;
+  };
