8c9f859da071bbd2d82461556ecd6e993ba65b5a
Quintessa Anderson <quintessaanderson@Quintessas-MacBook-Pro.local>
2018-03-27 00:01:09 -0500

    seeds.sql imported to database, review form works, started code for retrieving numbers to generate charts

--- controller/routes/post-api-routes.js
+++ controller/routes/post-api-routes.js
@@ -5,24 +5,35 @@ module.exports = function(app) {
 // Add a review
 app.post("/api/new/review", function(req, res) {
     
-        console.log("Review Data:");
-        console.log(req.body);
+        // console.log("Review Data:");
+        // console.log(req.body);
 
         var index = req.body;
     
         db.Review.create({
-          bootcampName: index.bootcampName,
-          curriculum: parseInt(index.curriculum),
-          instructor: parseInt(index.instructor),
-          jobAssistance: parseInt(index.jobAssistance),
-          jobFound: index.jobFound, 
-          jobPosition: index.jobPosition,
-          salaryRange: parseInt(index.salaryRange),
-          overallExperience: parseInt(index.overallExperience)
+          bootcampName: req.body.bootcampName,
+          curriculum: parseInt(req.body.curriculum),
+          instructor: parseInt(req.body.instructor),
+          jobAssistance: parseInt(req.body.jobAssistance),
+          jobFound: req.body.jobFound, 
+          jobPosition: req.body.jobPosition,
+          salaryRange: parseInt(req.body.salaryRange),
+          overallExperience: parseInt(req.body.overallExperience)
         }).then(function(results) {
-          console.log(results);
-          // `results` here would be the newly created review
-        //   res.end();
+
+        // var newChart = {
+        //   userExperience: results[i].overallExperience,
+        //   userJob: results[i].jobPosition,
+        //   userSalary: result[i].salaryRange
+        // }
+          // for (var i=0; i<results.length; i++){
+        
+            // console.log(results[i].overallExperience);
+            // console.log(results[i].jobPosition);
+            // console.log(results[i].salaryRange);  
+
+        // }
+         
           return res.redirect("/");
         });
     
