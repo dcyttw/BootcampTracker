b11465ef07d7101521ba1310d7ea6a591a68c1e4
Eric <ericsteinman@me.com>
2018-03-30 13:56:25 -0500

    Had to fix seeds.sql file, works fine now

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,119 +1,121 @@
-INSERT INTO reviews (bootcampName, curriculum, instructor, jobAssistance, jobFound, jobPosition, salaryRange, overallExperience)
-VALUES 
-('Fullstack Academy', 5, 5, 5, 'Yes', 'Front End Developer', '70000 - 80000', 5);
+USE  tracker_db;
 
-INSERT INTO reviews (bootcampName, curriculum, instructor, jobAssistance, jobFound, jobPosition, salaryRange, overallExperience)
+INSERT INTO reviews (bootcampName, curriculum, instructor, jobAssistance, jobFound, jobPosition, salaryRange, overallExperience, createdAt, updatedAt)
 VALUES 
-('Fullstack Academy', 5, 5, 5, 'No', 'Graduate', '40000 - 50000', 5);
+('Fullstack Academy', 5, 5, 5, 'Yes', 'Front End Developer', '70000 - 80000', 5, '1000-01-01 00:00:00', '1000-01-01 00:00:00');
 
-INSERT INTO reviews (bootcampName, curriculum, instructor, jobAssistance, jobFound, jobPosition, salaryRange, overallExperience)
+INSERT INTO reviews (bootcampName, curriculum, instructor, jobAssistance, jobFound, jobPosition, salaryRange, overallExperience, createdAt, updatedAt)
 VALUES 
-('Fullstack Academy', 5, 5, 5, 'Yes', 'Software Engineer', ' 80000+', 5);
+('Fullstack Academy', 5, 5, 5, 'No', 'Graduate', '40000 - 50000', 5, '1000-01-01 00:00:00', '1000-01-01 00:00:00');
 
-INSERT INTO reviews (bootcampName, curriculum, instructor, jobAssistance, jobFound, jobPosition, salaryRange, overallExperience)
+INSERT INTO reviews (bootcampName, curriculum, instructor, jobAssistance, jobFound, jobPosition, salaryRange, overallExperience, createdAt, updatedAt)
 VALUES 
-('Fullstack Academy', 4, 4, 4, 'Yes', 'Front End Developer', '70000 - 80000', 4);
+('Fullstack Academy', 5, 5, 5, 'Yes', 'Software Engineer', ' 80000+', 5, '1000-01-01 00:00:00', '1000-01-01 00:00:00');
 
-INSERT INTO reviews (bootcampName, curriculum, instructor, jobAssistance, jobFound, jobPosition, salaryRange, overallExperience)
+INSERT INTO reviews (bootcampName, curriculum, instructor, jobAssistance, jobFound, jobPosition, salaryRange, overallExperience, createdAt, updatedAt)
 VALUES 
-('Fullstack Academy', 3, 4, 4, 'No', 'Graduate', '40000 - 50000', 3);
+('Fullstack Academy', 4, 4, 4, 'Yes', 'Front End Developer', '70000 - 80000', 4, '1000-01-01 00:00:00', '1000-01-01 00:00:00');
 
-INSERT INTO reviews (bootcampName, curriculum, instructor, jobAssistance, jobFound, jobPosition, salaryRange, overallExperience)
+INSERT INTO reviews (bootcampName, curriculum, instructor, jobAssistance, jobFound, jobPosition, salaryRange, overallExperience, createdAt, updatedAt)
 VALUES 
-('Coding Dojo', 5, 5, 5, 'Yes', 'Dev Ops Engineer', '60000 - 70000', 5);
+('Fullstack Academy', 3, 4, 4, 'No', 'Graduate', '40000 - 50000', 3, '1000-01-01 00:00:00', '1000-01-01 00:00:00');
 
-INSERT INTO reviews (bootcampName, curriculum, instructor, jobAssistance, jobFound, jobPosition, salaryRange, overallExperience)
+INSERT INTO reviews (bootcampName, curriculum, instructor, jobAssistance, jobFound, jobPosition, salaryRange, overallExperience, createdAt, updatedAt)
 VALUES 
-('Coding Dojo', 5, 5, 5, 'No', 'Graduate', '40000 - 50000', 3);
+('Coding Dojo', 5, 5, 5, 'Yes', 'Dev Ops Engineer', '60000 - 70000', 5, '1000-01-01 00:00:00', '1000-01-01 00:00:00');
 
-INSERT INTO reviews (bootcampName, curriculum, instructor, jobAssistance, jobFound, jobPosition, salaryRange, overallExperience)
+INSERT INTO reviews (bootcampName, curriculum, instructor, jobAssistance, jobFound, jobPosition, salaryRange, overallExperience, createdAt, updatedAt)
 VALUES 
-('Coding Dojo', 5, 5, 5, 'Yes', 'Software Engineer', '80000+', 5);
+('Coding Dojo', 5, 5, 5, 'No', 'Graduate', '40000 - 50000', 3, '1000-01-01 00:00:00', '1000-01-01 00:00:00');
 
-INSERT INTO reviews (bootcampName, curriculum, instructor, jobAssistance, jobFound, jobPosition, salaryRange, overallExperience)
+INSERT INTO reviews (bootcampName, curriculum, instructor, jobAssistance, jobFound, jobPosition, salaryRange, overallExperience, createdAt, updatedAt)
 VALUES 
-('Coding Dojo', 4, 4, 3, 'No', 'Graduate', '40000 - 50000', 3);
+('Coding Dojo', 5, 5, 5, 'Yes', 'Software Engineer', '80000+', 5, '1000-01-01 00:00:00', '1000-01-01 00:00:00');
 
-INSERT INTO reviews (bootcampName, curriculum, instructor, jobAssistance, jobFound, jobPosition, salaryRange, overallExperience)
+INSERT INTO reviews (bootcampName, curriculum, instructor, jobAssistance, jobFound, jobPosition, salaryRange, overallExperience, createdAt, updatedAt)
 VALUES 
-('Coding Dojo', 2, 2, 3, 'No', 'Graduate', '40000 - 50000', 3);
+('Coding Dojo', 4, 4, 3, 'No', 'Graduate', '40000 - 50000', 3, '1000-01-01 00:00:00', '1000-01-01 00:00:00');
 
-INSERT INTO reviews (bootcampName, curriculum, instructor, jobAssistance, jobFound, jobPosition, salaryRange, overallExperience)
+INSERT INTO reviews (bootcampName, curriculum, instructor, jobAssistance, jobFound, jobPosition, salaryRange, overallExperience, createdAt, updatedAt)
 VALUES 
-('Fullstack Academy', 5, 5, 5, 'Yes', 'Software Developer', '70000 - 80000', 5);
+('Coding Dojo', 2, 2, 3, 'No', 'Graduate', '40000 - 50000', 3, '1000-01-01 00:00:00', '1000-01-01 00:00:00');
 
-INSERT INTO reviews (bootcampName, curriculum, instructor, jobAssistance, jobFound, jobPosition, salaryRange, overallExperience)
+INSERT INTO reviews (bootcampName, curriculum, instructor, jobAssistance, jobFound, jobPosition, salaryRange, overallExperience, createdAt, updatedAt)
 VALUES 
-('Fullstack Academy', 5, 4, 4, 'Yes', 'Web Developer', '60000 - 70000', 5);
+('Fullstack Academy', 5, 5, 5, 'Yes', 'Software Developer', '70000 - 80000', 5, '1000-01-01 00:00:00', '1000-01-01 00:00:00');
 
-INSERT INTO reviews (bootcampName, curriculum, instructor, jobAssistance, jobFound, jobPosition, salaryRange, overallExperience)
+INSERT INTO reviews (bootcampName, curriculum, instructor, jobAssistance, jobFound, jobPosition, salaryRange, overallExperience, createdAt, updatedAt)
 VALUES 
-('Fullstack Academy', 5, 5, 5, 'Yes', 'Software Developer', '70000 - 80000', 5);
+('Fullstack Academy', 5, 4, 4, 'Yes', 'Web Developer', '60000 - 70000', 5, '1000-01-01 00:00:00', '1000-01-01 00:00:00');
 
-INSERT INTO reviews (bootcampName, curriculum, instructor, jobAssistance, jobFound, jobPosition, salaryRange, overallExperience)
+INSERT INTO reviews (bootcampName, curriculum, instructor, jobAssistance, jobFound, jobPosition, salaryRange, overallExperience, createdAt, updatedAt)
 VALUES 
-('Fullstack Academy', 5, 5, 4, 'Yes', 'Software Engineer', '80000+', 5);
+('Fullstack Academy', 5, 5, 5, 'Yes', 'Software Developer', '70000 - 80000', 5, '1000-01-01 00:00:00', '1000-01-01 00:00:00');
 
-INSERT INTO reviews (bootcampName, curriculum, instructor, jobAssistance, jobFound, jobPosition, salaryRange, overallExperience)
+INSERT INTO reviews (bootcampName, curriculum, instructor, jobAssistance, jobFound, jobPosition, salaryRange, overallExperience, createdAt, updatedAt)
 VALUES 
-('Fullstack Academy', 5, 5, 5, 'Yes', 'Software Developer', '80000+', 5);
+('Fullstack Academy', 5, 5, 4, 'Yes', 'Software Engineer', '80000+', 5, '1000-01-01 00:00:00', '1000-01-01 00:00:00');
 
-INSERT INTO reviews (bootcampName, curriculum, instructor, jobAssistance, jobFound, jobPosition, salaryRange, overallExperience)
+INSERT INTO reviews (bootcampName, curriculum, instructor, jobAssistance, jobFound, jobPosition, salaryRange, overallExperience, createdAt, updatedAt)
 VALUES 
-('Actualize', 5, 5, 5, 'Yes', 'Web Developer', '60000 - 70000', 5);
+('Fullstack Academy', 5, 5, 5, 'Yes', 'Software Developer', '80000+', 5, '1000-01-01 00:00:00', '1000-01-01 00:00:00');
 
-INSERT INTO reviews (bootcampName, curriculum, instructor, jobAssistance, jobFound, jobPosition, salaryRange, overallExperience)
+INSERT INTO reviews (bootcampName, curriculum, instructor, jobAssistance, jobFound, jobPosition, salaryRange, overallExperience, createdAt, updatedAt)
 VALUES 
-('Actualize', 5, 5, 5, 'Yes', 'Software Engineer', '80000+', 5);
+('Actualize', 5, 5, 5, 'Yes', 'Web Developer', '60000 - 70000', 5, '1000-01-01 00:00:00', '1000-01-01 00:00:00');
 
-INSERT INTO reviews (bootcampName, curriculum, instructor, jobAssistance, jobFound, jobPosition, salaryRange, overallExperience)
+INSERT INTO reviews (bootcampName, curriculum, instructor, jobAssistance, jobFound, jobPosition, salaryRange, overallExperience, createdAt, updatedAt)
 VALUES 
-('Actualize', 5, 5, 5, 'Yes', 'Web Developer', '60000 - 70000', 5);
+('Actualize', 5, 5, 5, 'Yes', 'Software Engineer', '80000+', 5, '1000-01-01 00:00:00', '1000-01-01 00:00:00');
 
-INSERT INTO reviews (bootcampName, curriculum, instructor, jobAssistance, jobFound, jobPosition, salaryRange, overallExperience)
+INSERT INTO reviews (bootcampName, curriculum, instructor, jobAssistance, jobFound, jobPosition, salaryRange, overallExperience, createdAt, updatedAt)
 VALUES 
-('Actualize', 5, 5, 5, 'Yes', 'Web Developer', '60000 - 70000', 5);
+('Actualize', 5, 5, 5, 'Yes', 'Web Developer', '60000 - 70000', 5, '1000-01-01 00:00:00', '1000-01-01 00:00:00');
 
-INSERT INTO reviews (bootcampName, curriculum, instructor, jobAssistance, jobFound, jobPosition, salaryRange, overallExperience)
+INSERT INTO reviews (bootcampName, curriculum, instructor, jobAssistance, jobFound, jobPosition, salaryRange, overallExperience, createdAt, updatedAt)
 VALUES 
-('Actualize', 2, 4, 1, 'No', 'Graduate', '40000 - 50000', 3);
+('Actualize', 5, 5, 5, 'Yes', 'Web Developer', '60000 - 70000', 5, '1000-01-01 00:00:00', '1000-01-01 00:00:00');
 
-INSERT INTO reviews (bootcampName, curriculum, instructor, jobAssistance, jobFound, jobPosition, salaryRange, overallExperience)
+INSERT INTO reviews (bootcampName, curriculum, instructor, jobAssistance, jobFound, jobPosition, salaryRange, overallExperience, createdAt, updatedAt)
 VALUES 
-('Coding Temple', 5, 5, 5, 'Yes', 'Application Engineer', '60000 - 70000', 5);
+('Actualize', 2, 4, 1, 'No', 'Graduate', '40000 - 50000', 3, '1000-01-01 00:00:00', '1000-01-01 00:00:00');
 
-INSERT INTO reviews (bootcampName, curriculum, instructor, jobAssistance, jobFound, jobPosition, salaryRange, overallExperience)
+INSERT INTO reviews (bootcampName, curriculum, instructor, jobAssistance, jobFound, jobPosition, salaryRange, overallExperience, createdAt, updatedAt)
 VALUES 
-('Coding Temple', 5, 5, 5, 'Yes', 'Web Developer', '60000 - 70000', 5);
+('Coding Temple', 5, 5, 5, 'Yes', 'Application Engineer', '60000 - 70000', 5, '1000-01-01 00:00:00', '1000-01-01 00:00:00');
 
-INSERT INTO reviews (bootcampName, curriculum, instructor, jobAssistance, jobFound, jobPosition, salaryRange, overallExperience)
+INSERT INTO reviews (bootcampName, curriculum, instructor, jobAssistance, jobFound, jobPosition, salaryRange, overallExperience, createdAt, updatedAt)
 VALUES 
-('Coding Temple', 1, 2, 1, 'No', 'Graduate', '40000 - 50000', 1);
+('Coding Temple', 5, 5, 5, 'Yes', 'Web Developer', '60000 - 70000', 5, '1000-01-01 00:00:00', '1000-01-01 00:00:00');
 
-INSERT INTO reviews (bootcampName, curriculum, instructor, jobAssistance, jobFound, jobPosition, salaryRange, overallExperience)
+INSERT INTO reviews (bootcampName, curriculum, instructor, jobAssistance, jobFound, jobPosition, salaryRange, overallExperience, createdAt, updatedAt)
 VALUES 
-('Coding Temple', 4, 5, 4, 'Yes', 'Software Developer', '70000 - 80000', 4);
+('Coding Temple', 1, 2, 1, 'No', 'Graduate', '40000 - 50000', 1, '1000-01-01 00:00:00', '1000-01-01 00:00:00');
 
-INSERT INTO reviews (bootcampName, curriculum, instructor, jobAssistance, jobFound, jobPosition, salaryRange, overallExperience)
+INSERT INTO reviews (bootcampName, curriculum, instructor, jobAssistance, jobFound, jobPosition, salaryRange, overallExperience, createdAt, updatedAt)
 VALUES 
-('Coding Temple', 5, 3, 4, 'Yes', 'Fullstack Developer', '70000 - 80000', 4);
+('Coding Temple', 4, 5, 4, 'Yes', 'Software Developer', '70000 - 80000', 4, '1000-01-01 00:00:00', '1000-01-01 00:00:00');
 
-INSERT INTO reviews (bootcampName, curriculum, instructor, jobAssistance, jobFound, jobPosition, salaryRange, overallExperience)
+INSERT INTO reviews (bootcampName, curriculum, instructor, jobAssistance, jobFound, jobPosition, salaryRange, overallExperience, createdAt, updatedAt)
 VALUES 
-('Northwestern Boot Camp', 5, 5, 5, 'No', 'Graduate', '40000 - 50000', 5);
+('Coding Temple', 5, 3, 4, 'Yes', 'Fullstack Developer', '70000 - 80000', 4, '1000-01-01 00:00:00', '1000-01-01 00:00:00');
 
-INSERT INTO reviews (bootcampName, curriculum, instructor, jobAssistance, jobFound, jobPosition, salaryRange, overallExperience)
+INSERT INTO reviews (bootcampName, curriculum, instructor, jobAssistance, jobFound, jobPosition, salaryRange, overallExperience, createdAt, updatedAt)
 VALUES 
-('Northwestern Boot Camp', 5, 5, 4, 'No', 'Graduate', '40000 - 50000', 5);
+('Northwestern Boot Camp', 5, 5, 5, 'No', 'Graduate', '40000 - 50000', 5, '1000-01-01 00:00:00', '1000-01-01 00:00:00');
 
-INSERT INTO reviews (bootcampName, curriculum, instructor, jobAssistance, jobFound, jobPosition, salaryRange, overallExperience)
+INSERT INTO reviews (bootcampName, curriculum, instructor, jobAssistance, jobFound, jobPosition, salaryRange, overallExperience, createdAt, updatedAt)
 VALUES 
-('Northwestern Boot Camp', 1, 1, 1, 'No', 'Graduate', '40000 - 50000', 1);
+('Northwestern Boot Camp', 5, 5, 4, 'No', 'Graduate', '40000 - 50000', 5, '1000-01-01 00:00:00', '1000-01-01 00:00:00');
 
-INSERT INTO reviews (bootcampName, curriculum, instructor, jobAssistance, jobFound, jobPosition, salaryRange, overallExperience)
+INSERT INTO reviews (bootcampName, curriculum, instructor, jobAssistance, jobFound, jobPosition, salaryRange, overallExperience, createdAt, updatedAt)
 VALUES 
-('Northwestern Boot Camp', 2, 2, 1, 'No', 'Graduate', '40000 - 50000', 1);
+('Northwestern Boot Camp', 1, 1, 1, 'No', 'Graduate', '40000 - 50000', 1, '1000-01-01 00:00:00', '1000-01-01 00:00:00');
 
-INSERT INTO reviews (bootcampName, curriculum, instructor, jobAssistance, jobFound, jobPosition, salaryRange, overallExperience)
+INSERT INTO reviews (bootcampName, curriculum, instructor, jobAssistance, jobFound, jobPosition, salaryRange, overallExperience, createdAt, updatedAt)
 VALUES 
-('Northwestern Boot Camp', 5, 5, 5, 'No', 'Graduate', '40000 - 50000', 5);
+('Northwestern Boot Camp', 2, 2, 1, 'No', 'Graduate', '40000 - 50000', 1, '1000-01-01 00:00:00', '1000-01-01 00:00:00');
+
+INSERT INTO reviews (bootcampName, curriculum, instructor, jobAssistance, jobFound, jobPosition, salaryRange, overallExperience, createdAt, updatedAt)
+VALUES 
+('Northwestern Boot Camp', 5, 5, 5, 'No', 'Graduate', '40000 - 50000', 5, '1000-01-01 00:00:00', '1000-01-01 00:00:00');
